4a9bef3312dcefb4accccf9d36c8b1894f5569d2
Dale <498971746@qq.com>
2022-07-03 15:57:03 +0800

    添加 eBook 项目

---
+++ server/auth/src/main/java/com/jdy/angel/server/AuthApplication.java
@@ -0,0 +1,19 @@
+package com.jdy.angel.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 认证服务
+ *
+ * @author Dale
+ * @create 2021/10/13 16:47
+ */
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
---
+++ server/ebook/src/main/java/com/jdy/angel/server/EbookApplication.java
@@ -0,0 +1,19 @@
+package com.jdy.angel.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 电子书服务
+ *
+ * @author Dale
+ * @create 2021/10/13 16:47
+ */
+@SpringBootApplication
+public class EbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookApplication.class, args);
+	}
+
+}
